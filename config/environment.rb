678010df0a26748a56fc883ea445c8f8fcba1b57
Karl Varga <kjvarga@gmail.com>
2010-03-08 00:09:18 -0800

    Use friendly_id and hoptoad_notifier. Use Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Lines beginning with '#prod' will be uncommented in production by capistrano
 #prod ENV['RAILS_ENV'] ||= 'production'
@@ -14,9 +14,11 @@ Rails::Initializer.run do |config|
   config.gem "mechanize", :version => ">=0.9.3"
   config.gem "haml", :version => ">=2.0.9"
   config.gem "whenever", :version => ">=0.3.6", :lib => false
-  config.gem 'sitemap_generator', :source => 'http://gemcutter.org'
+  config.gem 'sitemap_generator'
+  config.gem "friendly_id", :version => ">= 2.3.0"
+  config.gem 'hoptoad_notifier'
   
-  config.time_zone = 'Perth'
+  config.time_zone = 'Vancouver'
 
   config.action_mailer.register_template_extension('haml')
 end
