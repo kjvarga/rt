0ea1974748ccfba76f309e0f56027946bfd23c97
Karl Varga <kjvarga@gmail.com>
2011-01-05 23:46:00 -0800

    Protect the site with http basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,20 @@ class ApplicationController < ActionController::Base
   include ExceptionNotifiable
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
+
+  before_filter :require_authenticated_user
+
   def test_mailer
     render :text => '<h1>fuck off you git!</h1>', :status => 500
   end
-  
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def require_authenticated_user
+    return if authenticate_or_request_with_http_basic do |ident_string, secret_string|
+      ident_string == 'karl' && secret_string.to_s.crypt(ident_string)
+    end
+    render :text => "You must be logged in to view this site", :layout => false, :status => status
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ Rails::Initializer.run do |config|
   config.gem "nokogiri", :version => ">=1.3.1"
   config.gem "mechanize", :version => ">=0.9.3"
   config.gem "haml", :version => ">=2.0.9"
-  config.gem "whenever", :version => ">=0.3.6", :lib => false
+  #config.gem "whenever", :version => ">=0.3.6", :lib => false
   config.gem 'sitemap_generator', :source => 'http://gemcutter.org'
   
   config.time_zone = 'Perth'
